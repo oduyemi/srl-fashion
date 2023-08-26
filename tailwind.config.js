8c29a83e69d947a6310a50cfaf098c38b6c298eb
Yemi Oduyemi <yhermii@gmail.com>
2023-08-26 16:03:38 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+],
+theme: {
+  extend: {
+    fontFamily: {
+      lato: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      butter: "#E4CB9C",
+      goldie: "#CD8F2C",
+      subtext: "#B0B0B0",
+      tan: "#352208",
+      brownie: "#564946",
+      ggreen: "#558564",
+      bluey: "#39A9DB",
+      placeholderColor: "#DEDEDE"
+    },
+  },
+},
+  plugins: [],
+}
+
