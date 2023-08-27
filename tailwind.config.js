27cbaa07ace61e9f381bf6d362d9982dbcad680a
Yemi Oduyemi <yhermii@gmail.com>
2023-08-27 20:03:47 +0100

    Revert "update"
    This reverts commit 91d2e5bf71f427edbab547c5e8b3253d84ea6da7.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,6 @@ theme: {
       bluey: "#39A9DB",
       placeholderColor: "#DEDEDE"
     },
-    variants: {
-      display: ['responsive', 'group-hover', 'group-focus'],
-     },
   },
 },
   plugins: [],
